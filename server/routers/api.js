3e813b94821ffc763d3753135bead9b4848309a3
renwofei423 <renwofei423@163.com>
2023-07-04 00:14:57 +0800

    feat: 动态域名设置
    * 支付中可通过环境变量动态域名设置

--- server/routers/api.js
+++ server/routers/api.js
@@ -970,7 +970,8 @@ router.post('/pay/precreate', async (req, res, next) => {
     //   }
     //   return `${req.protocol}://${host.split(':')[0]}`
     // }
-    const notifyUrl = `https://${req.get('host')?.split(':')[0]}/api/pay/notify?channel=${paymentInfo.channel}`;
+    const hostname = process.env.HOSTNAME || req.get('host').split(':')[0];
+    const notifyUrl = `https://${hostname}/api/pay/notify?channel=${paymentInfo.channel}`;
     const amount = productInfo.price / 100;
     const paymentParams = JSON.parse(paymentInfo.params);
     const paramsStringify = JSON.stringify({
